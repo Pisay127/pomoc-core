e4d07f53d93bdceb0b6b4816e6dfd475199ad052
Sean Francis N. Ballais <sean@seanballais.com>
2017-09-14 22:38:45 +0800

    Fixed a time bug, and a LAN access bug where the server cannot be accessed within the LAN.

--- bin/pomoc-core.py
+++ bin/pomoc-core.py
@@ -4,10 +4,10 @@
 import sys
 import os
 import time
-import signal
-
 from datetime import datetime
-from datetime import timezone
+from wsgiref import simple_server
+
+from pomoccore.api import api
 
 
 server_start_time = int(round(time.time() * 1000))
@@ -16,21 +16,37 @@ server_start_time = int(round(time.time() * 1000))
 def main():
     module_dir = os.path.abspath(__file__)[:(-1 * len('bin/pomoc-core.py'))]
     sys.path.append(module_dir)
-    signal.signal(signal.SIGINT, _sigint_handler)
 
-    from wsgiref import simple_server
-    from pomoccore.api import api
+    _print_server_message('{0} Starting Pomoc Core server at 0.0.0.0:8080'.format(_get_current_datetime()), True)
 
-    httpd = simple_server.make_server('127.0.0.1', 8080, api)
+    httpd = simple_server.make_server('0.0.0.0', 8080, api)
 
-    print("{0} Serving Pomoc Core at 127.0.0.1:8080...".format(_get_current_datetime()))
+    _print_server_message('{0} Serving Pomoc Core at localhost:8080...'.format(_get_current_datetime()), True)
 
     httpd.serve_forever()
     httpd.server_close()
 
 
+def _print_server_message(message, postmessage_line=False):
+    line_string = ('-' * len(message)) + ('-' * 3)
+    print(line_string)
+    print(message)
+
+    if postmessage_line:
+        print(line_string)
+
+
+def _get_local_time():
+    return datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def _get_current_utc_offset():
+    offset = (-time.timezone // 3600) * 100
+    return '{0}{1:04d}'.format('+' if offset > 0 else '-', abs(offset))
+
+
 def _get_current_datetime():
-    return datetime.now(timezone.utc).strftime("[%Y-%m-%d %H:%M:%S %z]")
+    return '[{0} {1}]'.format(_get_local_time(), _get_current_utc_offset())
 
 
 def _get_timelapse(end_time, start_time):
@@ -40,17 +56,20 @@ def _get_timelapse(end_time, start_time):
     minutes, milliseconds = divmod(milliseconds, 60000)
     seconds, milliseconds = divmod(milliseconds, 1000)
 
-    elapsed_time = "{0} d, {1} hr, {2} min, {3} s, {4} ms".format(days, hours, minutes, seconds, milliseconds)
+    elapsed_time = '{0} d, {1} hr, {2} min, {3} s, {4} ms'.format(days, hours, minutes, seconds, milliseconds)
     return elapsed_time
 
 
-def _sigint_handler(signal, frame):
+def _handle_keyboard_interrupt():
     time_lapsed = _get_timelapse(int(round(time.time() * 1000)), server_start_time)
-    print("\n{0} Shutting local Pomoc Core server down...".format(_get_current_datetime()))
-    print("----------------------------------------")
-    print("Server ran for {0}...".format(time_lapsed))
+    print('')  # Note that this automatically prints a new line.
+    _print_server_message('{0} Shutting local Pomoc Core server down...'.format(_get_current_datetime()))
+    _print_server_message('Server ran for {0}...'.format(time_lapsed))
     sys.exit(0)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        _handle_keyboard_interrupt()
--- pomoccore/controllers/user_accounts.py
+++ pomoccore/controllers/user_accounts.py
@@ -3,5 +3,13 @@
 
 
 class UsersController(object):
-    def on_post(self, req, resp):
+    def on_get(self, req, resp):
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'Grace Hopper'
+        }
 
+        resp.media = quote
