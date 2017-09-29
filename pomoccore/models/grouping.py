0ab4c4bb8ca4a6931bcf5f192ba1144f1e668b0f
Sean Francis N. Ballais <sean@seanballais.com>
2017-09-29 13:24:50 +0800

    Added additional section and batch models.

--- pomoccore/models/grouping.py
+++ pomoccore/models/grouping.py
@@ -39,5 +39,36 @@ class Section(BaseModel):
     def __repr__(self):
         return '<Section {0}'.format(self.section_name)
 
-# TODO: Add SectionAdvisor model
-# TODO: Add BatchAdvisor model
+
+class SectionAdvisor(BaseModel):
+
+    __tablename__ = 'section_advisor'
+
+    section_name = Column('section_name', Text, primary_key=True, nullable=False)
+    school_year = Column('school_year', Text, primary_key=True, nullable=False)
+    advisor = Column('advisor', Text, nullable=False)
+
+    def __init__(self, section_name, school_year, advisor):
+        self.section_name = section_name
+        self.school_year = school_year
+        self.advisor = advisor
+
+    def __repr__(self):
+        return '<SectionAdvisor {0} - {1} (S.Y. {2})>'.format(self.advisor, self.section_name, self.school_year)
+
+
+class BatchAdvisor(BaseModel):
+
+    __tablename__ = 'batch_advisor'
+
+    batch_year = Column('batch_year', Integer, primary_key=True, nullable=False)
+    school_year = Column('school_year', Text, primary_key=True, nullable=False)
+    advisor = Column('advisor', Text, primary_key=True, nullable=False)
+
+    def __init__(self, batch_year, school_year, advisor):
+        self.batch_year = batch_year
+        self.school_year = school_year
+        self.advisor = advisor
+
+    def __repr__(self):
+        return '<BatchAdvisor {0} - {1} (S.Y. {2})'.format(self.advisor, self.batch_year, self.school_year)
