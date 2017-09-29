51d24e99a4390a5bce79b949517965451446bef1
Sean Francis N. Ballais <sean@seanballais.com>
2017-09-29 12:34:54 +0800

    Added student models.

--- pomoccore/models/student.py
+++ pomoccore/models/student.py
@@ -6,6 +6,7 @@ from sqlalchemy import SmallInteger
 from sqlalchemy import Integer
 from sqlalchemy import Text
 from sqlalchemy import CHAR
+from sqlalchemy import DECIMAL
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy.schema import ForeignKeyConstraint
 
@@ -36,15 +37,15 @@ class StudentSection(BaseModel):
     __table_args__ = (ForeignKeyConstraint(['section_name', 'year_level'],
                                            ['section.section_name', 'section.year_level']),)
 
-    student_number = Column('student_number', Text,
+    student_id = Column('student_id', Text,
                             ForeignKey('student_account.id_number', ),
                             primary_key=True, nullable=False)
     section_name = Column('section_name', Text, primary_key=True, nullable=False)
     year_level = Column('year_level', SmallInteger, primary_key=True, nullable=False)
     school_year = Column('school_year', Text, primary_key=True, nullable=True)
 
-    def __init__(self, student_number, section_name, year_level, school_year):
-        self.student_number = student_number
+    def __init__(self, student_id, section_name, year_level, school_year):
+        self.student_id = student_id
         self.section_name = section_name
         self.year_level = year_level
         self.school_year = school_year
@@ -56,18 +57,18 @@ class StudentSection(BaseModel):
 class StudentRating(BaseModel):
 
     __tablename__ = 'student_rating'
-    __table_args__ = (ForeignKeyConstraint(['student_number'], ['student_character_rating_criteria.criterion_id']),
+    __table_args__ = (ForeignKeyConstraint(['student_id'], ['student_character_rating_criteria.criterion_id']),
                       ForeignKeyConstraint(['criterion_id']), ['student_account.id_number'],)
 
-    student_number = Column('student_number', Text, primary_key=True, nullable=False)
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
     criterion_id = Column('criterion_id', SmallInteger, primary_key=True, nullable=False)
     rating = Column('rating', CHAR, nullable=False)
     quarter = Column('quarter', SmallInteger, primary_key=True, nullable=False)
     year_level = Column('year_level', SmallInteger, primary_key=True, nullable=False)
     school_year = Column('school_year', Text, primary_key=True, nullable=False)
 
-    def __init__(self, student_number, criterion_id, rating, quarter, year_level, school_year):
-        self.student_number = student_number
+    def __init__(self, student_id, criterion_id, rating, quarter, year_level, school_year):
+        self.student_id = student_id
         self.criterion_id = criterion_id
         self.rating = rating
         self.quarter = quarter
@@ -75,20 +76,20 @@ class StudentRating(BaseModel):
         self.school_year = school_year
 
     def __repr__(self):
-        return '<StudentRating {0} - {1}'.format(self.student_number, self.criterion_id)
+        return '<StudentRating {0} - {1}'.format(self.student_id, self.criterion_id)
 
 
 class StudentBatch(BaseModel):
 
     __tablename__ = 'student_batch'
-    __table_args__ = (ForeignKeyConstraint(['student_number', 'batch_year'],
+    __table_args__ = (ForeignKeyConstraint(['student_id', 'batch_year'],
                                            ['student_account.id_number', 'batch.batch_year']),)
 
-    student_number = Column('student_number', Text, primary_key=True, nullable=False)
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
     batch_year = Column('batch_year', Integer, nullable=False)
 
-    def __init__(self, student_number, batch_year):
-        self.student_number = student_number
+    def __init__(self, student_id, batch_year):
+        self.student_id = student_id
         self.batch_year = batch_year
 
     def __repr__(self):
@@ -113,9 +114,9 @@ class StudentMonthlyRequiredDays(BaseModel):
 class StudentMonthlyAttendance(BaseModel):
 
     __tablename__ = 'student_monthly_attendance'
-    __table_args__ = (ForeignKeyConstraint(['student_number'], ['student_account.id_number']),)
+    __table_args__ = (ForeignKeyConstraint(['student_id'], ['student_account.id_number']),)
 
-    student_number = Column('student_number', Text, primary_key=True, nullable=False)
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
     month = Column('month', SmallInteger, primary_key=True, nullable=False)
     quarter = Column('quarter', SmallInteger, primary_key=True, nullable=False)
     year_level = Column('year_level', SmallInteger, primary_key=True, nullable=False)
@@ -125,9 +126,9 @@ class StudentMonthlyAttendance(BaseModel):
     days_tardy = Column('days_tardy', SmallInteger, nullable=False)
     days_absent = Column('days_absent', SmallInteger, nullable=False)
 
-    def __init__(self, student_number, month, quarter, year_level,
+    def __init__(self, student_id, month, quarter, year_level,
                  school_year, days_required, days_present, days_tardy, days_absent):
-        self.student_number = student_number
+        self.student_id = student_id
         self.month = month
         self.quarter = quarter
         self.year_level = year_level
@@ -138,31 +139,91 @@ class StudentMonthlyAttendance(BaseModel):
         self.days_absent = days_absent
 
     def __repr__(self):
-        return '<StudentMonthlyAttendance {0} - Month {1}>'.format(self.student_number, self.month)
+        return '<StudentMonthlyAttendance {0} - Month {1}>'.format(self.student_id, self.month)
 
 
 class StudentStatus(BaseModel):
 
     __tablename__ = 'student_status'
-    __table_args__ = (ForeignKeyConstraint(['student_number'], ['student_account.id_number']),)
+    __table_args__ = (ForeignKeyConstraint(['student_id'], ['student_account.id_number']),)
 
-    student_number = Column('student_number', Text, primary_key=True, nullable=False)
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
     status = Column('status', Text, primary_key=True, nullable=False)
     quarter = Column('quarter', SmallInteger, primary_key=True, nullable=False)
     year_level = Column('year_level', SmallInteger, primary_key=True, nullable=False)
     school_year = Column('school_year', Text, primary_key=True, nullable=False)
 
-    def __init__(self, student_number, status, quarter, year_level, school_year):
-        self.student_number = student_number
+    def __init__(self, student_id, status, quarter, year_level, school_year):
+        self.student_id = student_id
         self.status = status
         self.quarter = quarter
         self.year_level = year_level
         self.school_year = school_year
 
     def __repr__(self):
-        return '<StudentStatus {0} - {1}>'.format(self.student_number, self.quarter)
+        return '<StudentStatus {0} - {1}>'.format(self.student_id, self.quarter)
 
-# TODO: Add StudentSubject model
-# TODO: Add StudentSubjectGrade model
-# TODO: Add StudentSubjectGradeApproval model
 
+class StudentSubject(BaseModel):
+
+    __tablename__ = 'student_subject'
+
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
+    subject_name = Column('student_name', Text, primary_key=True, nullable=False)
+    school_year = Column('school_year', Text, primary_key=True, nullable=False)
+    instructor = Column('instructor', Text, nullable=False)
+
+    def __init__(self, student_id, subject_name, school_year, instructor):
+        self.student_id = student_id
+        self.subject_name = subject_name
+        self.school_year = school_year
+        self.instructor = instructor
+
+    def __repr__(self):
+        return '<StudentSubject {0} - {1}'.format(self.subject_name, self.student_id)
+
+
+class StudentSubjectGrade(BaseModel):
+
+    __tablename__ = 'student_subject_grade'
+
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
+    subject_name = Column('subject_name', Text, primary_key=True, nullable=False)
+    school_year = Column('school_year', Text, primary_key=True, nullable=False)
+    quarter = Column('quarter', SmallInteger, primary_key=True, nullable=False)
+    grade = Column('grade', DECIMAL, nullable=False)
+
+    def __init__(self, student_id, subject_name, school_year, quarter, grade):
+        self.student_id = student_id
+        self.subject_name = subject_name
+        self.school_year = school_year
+        self.quarter = quarter
+        self.grade = grade
+
+    def __repr__(self):
+        return '<StudentSubjectGrade {0} ({1}) - {2}'.format(self.subject_name, self.grade, self.student_id)
+
+
+class StudentSubjectGradeApprovalNeeded(BaseModel):
+
+    __tablename__ = 'student_subject_grade_approval_needed'
+
+    student_id = Column('student_id', Text, primary_key=True, nullable=False)
+    subject_name = Column('subject_name', Text, primary_key=True, nullable=False)
+    requesting_teacher = Column('requesting_teacher', Text, nullable=False)
+    school_year = Column('school_year', Text, primary_key=True, nullable=False)
+    quarter = Column('quarter', SmallInteger, primary_key=True, nullable=False)
+    proposed_grade = Column('proposed_grade', DECIMAL, nullable=False)
+
+    def __init__(self, student_id, subject_name, requesting_teacher, school_year, quarter, proposed_quarter):
+        self.student_id = student_id
+        self.subject_name = subject_name
+        self.requesting_teacher = requesting_teacher
+        self.school_year = school_year
+        self.quarter = quarter
+        self.proposed_grade = proposed_quarter
+
+    def __repr__(self):
+        return '<StudentSubjectGradeApprovalNeeded {0} (*{1}) - {2}>'.format(self.subject_name,
+                                                                             self.proposed_grade,
+                                                                             self.student_id)
--- pomoccore/models/user.py
+++ pomoccore/models/user.py
@@ -17,7 +17,7 @@ class User(Base):
 
     __abstract__ = True
 
-    id_number = Column('id', Text, primary_key=True, nullable=False, unique=True)
+    id = Column('id', Text, primary_key=True, nullable=False, unique=True)
     username = Column('username', Text, nullable=False, unique=True)
     password = Column('password', PasswordType(schemes=settings.PASSWORD_SCHEMES), nullable=False)
     first_name = Column('first_name', Text, nullable=False)
@@ -27,10 +27,10 @@ class User(Base):
     birth_date = Column('birth_date', DateTime, nullable=False)
     profile_picture = Column('profile_picture', Text, nullable=False, default='{directory to default image}')
 
-    def __init__(self, id_number, username, password, first_name,
+    def __init__(self, id, username, password, first_name,
                  middle_name, last_name, age, birth_date, profile_picture=None):
 
-        self.id_number = id_number.strip()
+        self.id = id.strip()
         self.username = username.strip().lower()
         self.password = password.strip()
         self.first_name = first_name.strip()
