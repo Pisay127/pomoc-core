c803f41bab3972a839fdc97e94807ca04e787d96
Sean Francis N. Ballais <sean@seanballais.com>
2017-11-11 17:07:00 +0800

    Added Falcon Jsonify with a custom class for API HTTP errors so that we can validate the json as soon as we get them.

--- pomoccore/middleware/jsonify.py
+++ pomoccore/middleware/jsonify.py
@@ -1,2 +1,20 @@
 # Copyright (c) 2017 Pisay127. All rights reserved.
 # See the file 'LICENSE' for the full license governing this code.
+
+
+import falcon_jsonify
+
+from pomoccore.utils.errors import APIBadRequestError
+
+
+class APIJsonify(falcon_jsonify.Middleware):
+    """
+    This middleware is just the same as Falcon-Jsonify but with uses the custom HTTP errors (APIError classes)
+    of this API so that the response JSONs will be the consistent across all API endpoints.
+    """
+
+    def __init__(self):
+        super(APIJsonify, self).__init__(True)
+
+    def bad_request(self, title, description):
+        raise APIBadRequestError(title=title, description=description)
