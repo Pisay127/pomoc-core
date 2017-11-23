873cebec2e933c4943c0af999b0dad5ff4d1796d
Sean Francis N. Ballais <sean@seanballais.com>
2017-11-23 23:56:13 +0800

    Added attributes to allow flexibility in the information needed.

--- pomoccore/controllers/user_controller.py
+++ pomoccore/controllers/user_controller.py
@@ -10,26 +10,46 @@ from pomoccore.models import Teacher
 from pomoccore.models import Student
 from pomoccore.utils import validators
 from pomoccore.utils import response
+from pomoccore.utils import misc
 
 
 class UserController(object):
     @falcon.before(validators.user_exists)
     def on_get(self, req, resp):
-        retrieved_user = db.Session.query(User).filter_by(user_id=req.get_json('id')).one()
+        user = db.Session.query(User).filter_by(user_id=req.get_json('id')).one()
 
-        data = {
-            'user': {
-                'id': retrieved_user.id_number,
-                'user_type': retrieved_user.user_type,
-                'username': retrieved_user.username,
-                'first_name': retrieved_user.first_name,
-                'middle_name': retrieved_user.middle_name,
-                'last_name': retrieved_user.last_name,
-                'age': retrieved_user.age,
-                'birth_date': retrieved_user.birth_date.strftime('%Y-%m-%d %H:%M:%S.%f'),
-                'profile_picture': retrieved_user.profile_picture
-            }
-        }
+        data = dict()
+        data['user'] = dict()
+
+        requested_attribs = misc.get_requested_attributes(req.get_json('attributes'))
+
+        if requested_attribs:
+            if 'id' in requested_attribs:
+                data['user']['id'] = user.id_number
+
+            if 'user_type' in requested_attribs:
+                data['user']['user_type'] = user.user_type
+
+            if 'username' in requested_attribs:
+                data['user']['username'] = user.username
+
+            if 'first_name' in requested_attribs:
+                data['user']['first_name'] = user.first_name
+
+            if 'middle_name' in requested_attribs:
+                data['user']['middle_name'] = user.middle_name
+
+            if 'last_name' in requested_attribs:
+                data['user']['last_name'] = user.last_name
+
+            if 'age' in requested_attribs:
+                data['user']['age'] = user.age
+
+            if 'birth_date' in requested_attribs:
+                data['user']['birth_date'] = user.birth_date.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+            if 'profile_picture' in requested_attribs:
+                data['user']['profile_picture'] = user.profile_picture
 
         response.set_successful_response(
             resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code?',
@@ -73,33 +93,33 @@ class UserController(object):
 
     @falcon.before(validators.user_exists)
     def on_put(self, req, resp):
-        retrieved_user = db.Session.query(User).filter_by(user_id=req.get_json('id')).one()
+        user = db.Session.query(User).filter_by(user_id=req.get_json('id')).one()
 
         if 'id_number' in req.json:
-            retrieved_user.id_number = req.get_json('id_number')
+            user.id_number = req.get_json('id_number')
 
         # We should not modify the user type. Records will get fucked up.
 
         if 'username' in req.json:
-            retrieved_user.username = req.get_json('username').strip().lower()
+            user.username = req.get_json('username').strip().lower()
 
         if 'password' in req.json:
-            retrieved_user.password = req.get_json('password')
+            user.password = req.get_json('password')
 
         if 'first_name' in req.json:
-            retrieved_user.password = req.get_json('first_name').strip()
+            user.password = req.get_json('first_name').strip()
 
         if 'middle_name' in req.json:
-            retrieved_user.middle_name = req.get_json('middle_name').strip()
+            user.middle_name = req.get_json('middle_name').strip()
 
         if 'last_name' in req.json:
-            retrieved_user.last_name = req.get_json('last_name').strip()
+            user.last_name = req.get_json('last_name').strip()
 
         if 'age' in req.json:
-            retrieved_user.age = req.get_json('age')
+            user.age = req.get_json('age')
 
         if 'birth_date' in req.json:
-            retrieved_user.birth_date = req.get_json('birth_date')
+            user.birth_date = req.get_json('birth_date')
 
         # NOTE: Handle profile pictures in the future.
 
@@ -107,17 +127,17 @@ class UserController(object):
 
         response.set_successful_response(
             resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code again?',
-            'User updated successfully', 'User {0} has been updated.'.format(retrieved_user.username)
+            'User updated successfully', 'User {0} has been updated.'.format(user.username)
         )
 
     @falcon.before(validators.user_exists)
     def on_delete(self, req, resp):
-        retrieved_user = db.Session.query(User).filter_by(user_id=req.get_json('id')).one()
+        user = db.Session.query(User).filter_by(user_id=req.get_json('id')).one()
 
-        db.Session.delete(retrieved_user)
+        db.Session.delete(user)
         db.Session.commit()
 
         response.set_successful_response(
             resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code again?',
-            'User deleted successfully', 'User {0} has been deleted.'.format(retrieved_user.subject_name)
+            'User deleted successfully', 'User {0} has been deleted.'.format(user.username)
         )
