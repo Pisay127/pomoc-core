77e82e3ae567d51a93896d51910bee780e479830
Sean Francis N. Ballais <sean@seanballais.com>
2017-12-28 22:12:08 +0800

    Completed student controller
    Added student rating controller.
    
    Added additional controllers.
    
    Added StudentSubjectGrade controller.
    
    Added StudentSubjectPendingGrade controller.
    
    Corrected variable in format()

--- pomoccore/utils/validators/student.py
+++ pomoccore/utils/validators/student.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from pomoccore import db
 from pomoccore.models import Student
+from pomoccore.models import StudentMonthlyRequiredDays
 from pomoccore.utils.errors import APINotFoundError
 
 
@@ -15,4 +16,15 @@ def exists(req, resp, resource, params):
     try:
         db.Session.query(Student).filter_by(student_id=int(req.get_json('student_id'))).one()
     except NoResultFound:
-        raise APINotFoundError('Student could not be found', 'student does not exist, or used to be.')
+        raise APINotFoundError('Student could not be found', 'Student does not exist, or used to be.')
+
+
+def monthly_attendance_school_year_exists(req, resp, resource, params):
+    if req.get_json('school_year') == '__all__':
+        return
+
+    try:
+        db.Session.query(StudentMonthlyRequiredDays).filter_by(school_year=req.get_json('school_year')).one()
+    except NoResultFound:
+        raise APINotFoundError('School year could be found', 'Non-existent school year. You is from the future?')
+
