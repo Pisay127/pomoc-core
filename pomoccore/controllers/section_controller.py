1a46dc35e35987733d940fa17adad05e307d1c4d
Sean Francis N. Ballais <sean@seanballais.com>
2018-01-02 22:59:41 +0800

    Added section advisor.

--- pomoccore/controllers/section_controller.py
+++ pomoccore/controllers/section_controller.py
@@ -5,6 +5,7 @@ import falcon
 
 from pomoccore import db
 from pomoccore.models import Section
+from pomoccore.models import SectionAdvisor
 from pomoccore.utils import validators
 from pomoccore.utils import response
 from pomoccore.utils.errors import APIUnprocessableEntityError
@@ -92,5 +93,119 @@ class SectionController(object):
 
         response.set_successful_response(
             resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code again?',
-            'Section successfully', 'Section {0} has been deleted.'.format(section.section_name)
+            'Section successfully', 'Section {0} has been deleted.'.format(req.get_json('section_id'))
+        )
+
+
+class SectionAdvisorByYearController(object):
+    @falcon.before(validators.batch.exists)
+    def on_get(self, req, resp):
+        section_advisors = db.Session.query(SectionAdvisor).filter_by(
+            section_id=req.get_json('section_id')
+        ).all().order_by(SectionAdvisor.section_id.desc(),
+                         SectionAdvisor.school_year.desc())
+
+        data = dict()
+        data['section'] = dict()
+
+        ctr = 0
+        for section_advisor in section_advisors:
+            data['section'][ctr] = dict()
+            for scope in req.scope:
+                try:
+                    data['section'][ctr][scope] = getattr(section_advisor, scope)
+                except AttributeError:
+                    raise APIUnprocessableEntityError('Invalid scope \'{0}\''.format(scope),
+                                                      'Scope is not part of the student.')
+
+        response.set_successful_response(
+            resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code?',
+            'Successful section advisor data retrieval', 'Section advisor data successfully gathered.', data
+        )
+
+
+class SectionAdvisorController(object):  # This controller gets the advisor by section advisor.
+    @falcon.before(validators.teacher.exists)
+    def on_get(self, req, resp):
+        data = dict()
+        section_advisors = db.Session.query(SectionAdvisor).filter_by(
+            advisor=req.get_json('teacher_id')
+        ).all().order_by(SectionAdvisor.batch_year.desc(),
+                         SectionAdvisor.school_year.desc())
+
+        data = dict()
+        data['batch'] = dict()
+
+        ctr = 0
+        for section_advisor in section_advisors:
+            data['section'][ctr] = dict()
+            for scope in req.scope:
+                try:
+                    data['section'][ctr][scope] = getattr(section_advisor, scope)
+                except AttributeError:
+                    raise APIUnprocessableEntityError('Invalid scope \'{0}\''.format(scope),
+                                                      'Scope is not part of the student.')
+
+        response.set_successful_response(
+            resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code?',
+            'Successful section advisor data retrieval', 'Section advisor data successfully gathered.', data
+        )
+
+    @falcon.before(validators.oauth.access_token_valid)
+    @falcon.before(validators.oauth.access_token_user_exists)
+    @falcon.before(validators.section.exists)
+    @falcon.before(validators.teacher.exists)
+    @falcon.before(validators.admin.required)
+    def on_post(self, req, resp):
+        section_id = req.get_json('section_id')
+        school_year = req.get_json('school_year')
+        advisor_id = req.get_json('teacher_id')
+
+        db.Session.add(SectionAdvisor(section_id, school_year, advisor_id))
+        db.Session.commit()
+
+        response.set_successful_response(
+            resp, falcon.HTTP_201, 'Ignacio! Where is the damn internal code again?',
+            'Added new section advisor successfully', 'New section advisor has been added.'
+        )
+
+    @falcon.before(validators.oauth.access_token_valid)
+    @falcon.before(validators.oauth.access_token_user_exists)
+    @falcon.before(validators.admin.required)
+    @falcon.before(validators.teacher.exists)
+    @falcon.before(validators.teacher.new_exists)
+    @falcon.before(validators.batch.exists)
+    def on_put(self, req, resp):
+        section_advisor = db.Session.query(SectionAdvisor).filter_by(section_id=req.get_json('section_id'),
+                                                                     school_year=req.get_json('school_year'),
+                                                                     advisor=req.get_json('teacher_id')).one()
+
+        if 'teacher_id' in req.json:
+            section_advisor.advisor = req.get_json('new_teacher_id')
+
+        db.Session.commit()
+
+        response.set_successful_response(
+            resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code again?',
+            'Section advisor updated successfully',
+            'Section advisor has been updated.'
+        )
+
+    @falcon.before(validators.oauth.access_token_valid)
+    @falcon.before(validators.oauth.access_token_user_exists)
+    @falcon.before(validators.admin.required)
+    @falcon.before(validators.teacher.exists)
+    @falcon.before(validators.section.exists)
+    def on_delete(self, req, resp):
+        section_advisor = db.Session.query(SectionAdvisor).filter_by(section_id=req.get_json('section_id'),
+                                                                     school_year=req.get_json('school_year'),
+                                                                     advisor=req.get_json('teacher_id')).one()
+
+        db.Session.delete(section_advisor)
+        db.Session.commit()
+
+        response.set_successful_response(
+            resp, falcon.HTTP_200, 'Ignacio! Where is the damn internal code again?',
+            'Section advisor deleted successfully',
+            'Section advisor has been deleted.'
         )
